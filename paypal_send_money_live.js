9318b73c3f4b35c440f5aee6c45aa77246880c03
Kevin Davis <kjydavis3@gmail.com>
2018-02-26 16:31:40 -0500

    cleaned up API key handling

--- paypal_send_money_live.js
+++ paypal_send_money_live.js
@@ -3,10 +3,43 @@ var paypal = require('paypal-rest-sdk');
 const client_id = process.env.PAYPAL_CLIENT_ID
 const client_secret = process.env.PAYPAL_CLIENT_SECRET
 
+
 paypal.configure({
   'mode': 'live', //sandbox or live
   'client_id': client_id,
   'client_secret': client_secret
 });
 
-console.log(client_secret)
+function sendPaypalPayment(api, receiver_email, amount, currency='USD') {
+    var sender_batch_id = Math.random().toString(36).substring(9);
+    var create_payout_json = {
+        "sender_batch_header": {
+            "sender_batch_id": sender_batch_id,
+            "email_subject": "Kava Testing Paypal SDK"
+        },
+        "items": [
+            {
+                "recipient_type": "EMAIL",
+                "amount": {
+                    "value": 0.90,
+                    "currency": currency
+                },
+                "receiver": receiver_email,
+                "note": "The moon awaits.",
+                "sender_item_id": "kava_demo_2"
+            }
+        ]
+    };
+    var sync_mode = 'false';
+    api.payout.create(create_payout_json, sync_mode, function (error, payout) {
+        if (error) {
+            console.log(error.response);
+            throw error;
+        } else {
+            console.log("Create Single Payout Response");
+            console.log(payout);
+        }
+    });
+}
+
+
